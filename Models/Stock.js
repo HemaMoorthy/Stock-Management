798f73dbcefa3183c996745ecc5e00fa585ef9d6
Hema Moorthy <hemamoorthy8@gmail.com>
2023-03-29 21:18:55 +0530

    stock

--- Models/Stock.js
+++ Models/Stock.js
@@ -14,6 +14,16 @@ const StockSchema = new mongoose.Schema({
     amount:{
         type:Number,
         required:true
+    },
+    brand:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'brand',
+        required: true
+    },
+    category:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'category',
+        required:true
     }
 },{ timestamp:true })
 
